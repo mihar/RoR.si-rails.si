b08369fabb9132c1a2916d65646b489be3eec8a0
Srdjan Prodanovic <srdjan.prodanovic@gmail.com>
2010-03-15 14:50:05 +0100

    randomness and orderness and limitness

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,11 +6,10 @@ class HomeController < ApplicationController
     @recent_topics          = Topic.recent 6
     @recent_tweets          = Tweet.recent 3
     @recent_blogs           = Entry.recent 10
-    @recent_apps            = App.ordered("created_at desc").limited 6 
-    @recent_jobs            = Job.all
+    @recent_apps            = App.all.sort_by(&:rand)[0..6]     
+    @users                  = User.all.sort_by(&:rand)[0..6]
+    # @recent_jobs            = Job.all
     # @features_project       = Project.featured
-    
-    @users                  = User.ordered("updated_at desc").limited(6)
   end
   
   def events_and_presentations
@@ -24,9 +23,9 @@ class HomeController < ApplicationController
   end
   
   def developers_and_apps
-    @users                  = User.ordered("updated_at desc")
+    @users                  = User.all.sort_by(&:rand)
     @apps                   = App.all
-    @projects               = Project.recent.limited(15)
+    @projects               = Project.all.sort_by(&:rand)
   end
 
 end
