f25154da963ea4eeb65b287ed2e3e0f10ca63a32
Miha Rebernik <rasmajx@gmail.com>
2010-03-16 12:40:32 +0100

    Including Hoptoad notification on deploy.

--- config/deploy.rb
+++ config/deploy.rb
@@ -23,13 +23,29 @@ namespace :vlad do
     Rake::Task['vlad:update'].invoke
     Rake::Task['vlad:migrate'].invoke
     Rake::Task['vlad:start_app'].invoke
+    Rake::Task['vlad:notify_hoptoad'].invoke
   end
 
-  # desc "Cleanup SASS leftovers"
-  # remote_task :cleanup_sass do
-  #   puts "Cleaning up SASS leftovers..."
-  #   run "rm #{current_release}/public/stylesheets/*.css;"
-  # end
-  # 
-  # remote_task :start_app => :cleanup_sass
+  task :notify_hoptoad => [:git_user, :git_revision] do
+    notify_command = "rake hoptoad:deploy TO=#{rails_env} REVISION=#{current_sha} REPO=#{repository} USER='#{current_user}'" 
+    puts "Notifying Hoptoad of Deploy (#{notify_command})" 
+    `#{notify_command}`
+    puts "Hoptoad Notification Complete." 
+  end
+
+  remote_task :git_revision do
+    set :current_sha, run("cd #{File.join(scm_path, 'repo')}; git rev-parse origin/master").strip
+  end
+
+  task :git_user do
+    set :current_user, `git config --get user.name`.strip
+  end
+
+  desc "Cleanup SASS leftovers"
+  remote_task :cleanup_sass do
+    puts "Cleaning up SASS leftovers..."
+    run "rm #{current_release}/public/stylesheets/style.css;"
+  end
+  
+  remote_task :start_app => :cleanup_sass
 end
