8064a8e31a06e5e00e8ca618461e03352aea686d
Miha Rebernik <rasmajx@gmail.com>
2010-04-01 18:02:24 +0200

    Removing sentence_of

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,19 +1,4 @@
 module ApplicationHelper
-
-  def sentence_of(things, opts = {}, &block)
-    if block
-      things = things.collect do |thing|
-        capture(thing, &block)
-      end
-    end
-
-    if opts[:connector] == :or
-      things.to_sentence :last_word_connector => ", or ",
-                         :two_words_connector => " or "
-    else
-      things.to_sentence
-    end
-  end
   
   def restrict_to_owner(resource, &block)
     if current_user and (current_user.send("owns_#{resource.class.to_s.downcase}?", resource) or current_user.admin?)
